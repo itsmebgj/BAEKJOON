a8f359e1581226b0c0332a252ea09d40e71f8838
itsmebgj <bgj3088@gmail.com>
2024-08-14 22:02:27 +0900

    1008, 10869, 10998

---
+++ src/boj10869/Main.java
@@ -0,0 +1,22 @@
+package src.boj10869;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a,b;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        scanner.close();
+    }
+}
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
---
+++ src/boj10998/Main.java
@@ -0,0 +1,18 @@
+package src.boj10998;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    int a,b;
+    a = scanner.nextInt();
+    b = scanner.nextInt();
+
+    System.out.println(a*b);
+
+    scanner.close();
+    }
+}
+//두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
