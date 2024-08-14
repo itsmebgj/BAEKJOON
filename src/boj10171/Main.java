ed34db4b4dad7984f53bce7544196ccd2dd54197
itsmebgj <bgj3088@gmail.com>
2024-08-14 20:56:27 +0900

    10171

---
+++ src/boj10171/Main.java
@@ -0,0 +1,11 @@
+package src.boj10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+//고양이를 출력한다.
