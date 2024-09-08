a0247335ca9974510584471dca57ad0ee693c121
Geunjae Bae <bgj3088@gmail.com>
2024-09-08 20:05:13 +0900

    11021,2558

---
+++ src/boj2558/Main.java
@@ -0,0 +1,13 @@
+package src.boj2558;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+        sc.close();
+    }
+}
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
