4f3520bf551919cfb096ec1b64887a5ec2e5331d
Geunjae Bae <bgj3088@gmail.com>
2024-09-09 13:35:04 +0900

    11022

--- src/boj11022/Main.java
+++ src/boj11022/Main.java
@@ -1,14 +1,14 @@
-package src.boj11021;
+package src.boj11022;
 
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for (int i = 1; i<=T; i++){
+        for(int i =1; i<=T; i++){
             int A = sc.nextInt();
             int B = sc.nextInt();
-            System.out.printf("Case #%d: %d\n", i, (A+B));
+            System.out.printf("Case #%d: %d + %d = %d\n", i, A, B, A+B);
         }
         sc.close();
     }
